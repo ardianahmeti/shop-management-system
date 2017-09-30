df1cf46c31ea51cf712cc07b117eb96eb75e02d7
ardianahmeti <ardiann83@gmail.com>
2017-09-30 23:15:53 +0200

    product insert and update finished!

--- Shop_Management_System/src/gui/MainPage.java
+++ Shop_Management_System/src/gui/MainPage.java
@@ -3,7 +3,10 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
@@ -12,6 +15,7 @@ import java.awt.Toolkit;
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.border.MatteBorder;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
@@ -30,7 +34,11 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.table.DefaultTableModel;
+import javax.swing.border.LineBorder;
 
 
 
@@ -41,6 +49,10 @@ public class MainPage {
 	
 	private double s_width = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 	private double s_height = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+	private int pid;
+	
+	
+	
 	private DefaultTableModel model;
 	
 	private JTextField txtSearchProduct;
@@ -58,6 +70,10 @@ public class MainPage {
 	Connection conn = null;
 	ResultSet res = null;
 	PreparedStatement pst = null;
+	private JTextField txtProdId;
+	private JTextField txtProdName;
+	private JTextField txtProdStock;
+	private JTextField txtProdPrice;
 	
 	
 	
@@ -192,6 +208,155 @@ public class MainPage {
 		frmMyshop.getContentPane().add(pnlProducts);
 		pnlProducts.setLayout(null);
 		
+		JPanel pnlProductInfo = new JPanel();
+		pnlProductInfo.setVisible(false);
+		pnlProductInfo.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		pnlProductInfo.setBackground(new Color(0, 102, 153));
+		pnlProductInfo.setBounds(169, 43, 683, 291);
+		pnlProducts.add(pnlProductInfo);
+		pnlProductInfo.setLayout(null);
+		
+		JLabel lblTDhnatE = new JLabel("T\u00CB DH\u00CBNAT E PRODUKTIT");
+		lblTDhnatE.setFont(new Font("Courier New", Font.BOLD, 26));
+		lblTDhnatE.setForeground(Color.WHITE);
+		lblTDhnatE.setBounds(177, 41, 336, 30);
+		pnlProductInfo.add(lblTDhnatE);
+		
+		JLabel lblId = new JLabel("ID:");
+		lblId.setFont(new Font("Courier New", Font.BOLD, 14));
+		lblId.setForeground(Color.WHITE);
+		lblId.setBounds(27, 105, 24, 17);
+		pnlProductInfo.add(lblId);
+		
+		JLabel lblEmri = new JLabel("EMRI I PRODUKTIT:");
+		lblEmri.setFont(new Font("Courier New", Font.BOLD, 14));
+		lblEmri.setForeground(Color.WHITE);
+		lblEmri.setBounds(177, 105, 136, 17);
+		pnlProductInfo.add(lblEmri);
+		
+		JLabel lblSasiaNDepo = new JLabel("SASIA N\u00CB DEPO");
+		lblSasiaNDepo.setFont(new Font("Courier New", Font.BOLD, 14));
+		lblSasiaNDepo.setForeground(Color.WHITE);
+		lblSasiaNDepo.setBounds(395, 105, 104, 17);
+		pnlProductInfo.add(lblSasiaNDepo);
+		
+		JLabel lblCmimiPerCope = new JLabel("\u00C7MIMI P\u00CBR COP\u00CB");
+		lblCmimiPerCope.setFont(new Font("Courier New", Font.BOLD, 14));
+		lblCmimiPerCope.setForeground(Color.WHITE);
+		lblCmimiPerCope.setBounds(532, 105, 112, 17);
+		pnlProductInfo.add(lblCmimiPerCope);
+		
+		txtProdId = new JTextField();
+		txtProdId.setEditable(false);
+		txtProdId.setBounds(27, 133, 110, 25);
+		pnlProductInfo.add(txtProdId);
+		txtProdId.setColumns(10);
+		
+		txtProdName = new JTextField();
+		txtProdName.setBounds(167, 133, 197, 25);
+		pnlProductInfo.add(txtProdName);
+		txtProdName.setColumns(10);
+		
+		txtProdStock = new JTextField();
+		txtProdStock.setText("");
+		txtProdStock.setBounds(395, 133, 110, 25);
+		pnlProductInfo.add(txtProdStock);
+		txtProdStock.setColumns(10);
+		
+		txtProdPrice = new JTextField();
+		txtProdPrice.setText("");
+		txtProdPrice.setBounds(532, 133, 120, 25);
+		pnlProductInfo.add(txtProdPrice);
+		txtProdPrice.setColumns(10);
+		
+		JLabel lblX = new JLabel("");
+		lblX.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) 
+			{
+				txtProdId.setText("");
+				txtProdName.setText("");
+				txtProdStock.setText("");
+				txtProdPrice.setText("");
+				pnlProductInfo.setVisible(false);
+				
+			}
+		});
+		lblX.setIcon(new ImageIcon(MainPage.class.getResource("/images/xicon.jpg")));
+		lblX.setBounds(648, 5, 30, 30);
+		pnlProductInfo.add(lblX);
+		
+		JButton btnShtoProduktin = new JButton("Shto Produktin");
+		btnShtoProduktin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				try{
+					conn = db_connection.connectDB();
+					String insert = "insert into products (pname,pstock,pprice) values('"+txtProdName.getText()+" ','"+txtProdStock.getText()
+										+"','"+Double.parseDouble(txtProdPrice.getText())+"';";
+					pst = conn.prepareStatement(insert);
+					pst.execute();
+					pst.close();
+					
+					JOptionPane.showMessageDialog(null, "Produkti u shtua me sukses!");
+					
+					txtProdId.setText("");
+					txtProdName.setText("");
+					txtProdStock.setText("");
+					txtProdPrice.setText("");
+					updateTable();
+					pnlProductInfo.setVisible(false);
+					
+					}
+					catch (Exception eUpdate)
+					{
+						JOptionPane.showMessageDialog(null, "Të dhënat nuk mund të ruhen!\n Error: "+eUpdate.toString());
+					}
+			}
+		});
+		btnShtoProduktin.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(255, 255, 204)));
+		btnShtoProduktin.setBackground(new Color(0, 102, 153));
+		btnShtoProduktin.setFont(new Font("Courier New", Font.BOLD, 17));
+		btnShtoProduktin.setForeground(Color.WHITE);
+		btnShtoProduktin.setBounds(405, 187, 247, 30);
+		pnlProductInfo.add(btnShtoProduktin);
+		
+		JButton btnRuajTeDhenat = new JButton("Ruaj t\u00EB Dh\u00EBnat");
+		btnRuajTeDhenat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				try{
+				conn = db_connection.connectDB();
+				String update = "Update  products set pname='"+txtProdName.getText()+" ', pstock='"+txtProdStock.getText()
+									+"  ', pprice='"+Double.parseDouble(txtProdPrice.getText())+"' where pid=' "+txtProdId.getText()+"  ';";
+				pst = conn.prepareStatement(update);
+				pst.execute();
+				pst.close();
+				
+				JOptionPane.showMessageDialog(null, "Të dhënat u ruajtën me sukses!");
+				
+				txtProdId.setText("");
+				txtProdName.setText("");
+				txtProdStock.setText("");
+				txtProdPrice.setText("");
+				updateTable();
+				pnlProductInfo.setVisible(false);
+				
+				}
+				catch (Exception eUpdate)
+				{
+					JOptionPane.showMessageDialog(null, "Të dhënat nuk mund të ruhen!\n Error: "+eUpdate.toString());
+				}
+
+			}
+		});
+		btnRuajTeDhenat.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(255, 255, 204)));
+		btnRuajTeDhenat.setBackground(new Color(0, 102, 153));
+		btnRuajTeDhenat.setFont(new Font("Courier New", Font.BOLD, 17));
+		btnRuajTeDhenat.setForeground(Color.WHITE);
+		btnRuajTeDhenat.setBounds(405, 222, 247, 30);
+		pnlProductInfo.add(btnRuajTeDhenat);
+		
 		JLabel lblProductsList = new JLabel("LISTA E PRODUKTEVE");
 		lblProductsList.setFont(new Font("Courier New", Font.BOLD, 18));
 		lblProductsList.setForeground(new Color(0, 102, 153));
@@ -238,6 +403,38 @@ public class MainPage {
 		pnlProducts.add(spCart);
 		
 		JButton btnAddProduct = new JButton("Shto Produkt");
+		btnAddProduct.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				try{
+				
+					String sql = "SELECT * FROM products ORDER BY PID DESC LIMIT 1";
+					pst = conn.prepareStatement(sql);
+					res = pst.executeQuery();
+						while (res.next()) 
+						{
+							
+							txtProdId.setText(String.valueOf((Integer.parseInt(res.getString("PID"))+1)));
+						}
+					
+					txtProdName.setText("");
+					txtProdStock.setText("");
+					txtProdPrice.setText("");
+					
+					btnShtoProduktin.setEnabled(true);
+					btnShtoProduktin.setVisible(true);
+					
+					btnRuajTeDhenat.setEnabled(false);
+					btnRuajTeDhenat.setVisible(false);
+					pnlProductInfo.setVisible(true);
+					
+				}
+				catch (Exception e4)
+				{
+					
+				}
+			}
+		});
 		btnAddProduct.setFocusPainted(false);
 		btnAddProduct.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 102, 153), new Color(0, 102, 153), new Color(0, 102, 153), new Color(0, 102, 153)));
 		btnAddProduct.setBackground(Color.WHITE);
@@ -295,8 +492,56 @@ public class MainPage {
 		tblProducts.setFont(new Font("Courier New", Font.BOLD, 13));
 		spProductsList.setViewportView(tblProducts);
 		
-		
-		
+		JPopupMenu popupMenu = new JPopupMenu();
+		addPopup(tblProducts, popupMenu);
+
+		JMenuItem menuItem = new JMenuItem("Ndrysho");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{	
+				try{
+				DefaultTableModel model = (DefaultTableModel) tblProducts.getModel();
+				pid = ((int) (model.getValueAt(tblProducts.getSelectedRow(), 0)));
+				
+					
+				
+				try {
+					String sql = "select * from products where pid='" + pid + "'";
+					pst = conn.prepareStatement(sql);
+					res = pst.executeQuery();
+					while (res.next()) {
+						txtProdId.setText(res.getString("PID"));
+						txtProdName.setText(res.getString("pname"));
+						txtProdStock.setText(res.getString("pstock"));
+						txtProdPrice.setText(res.getString("pprice"));
+						
+						
+						
+					}
+					btnShtoProduktin.setEnabled(false);
+					btnShtoProduktin.setVisible(false);
+					
+					btnRuajTeDhenat.setEnabled(true);
+					btnRuajTeDhenat.setVisible(true);
+					pnlProductInfo.setVisible(true);
+					pst.close();
+
+				} catch (Exception e2) {
+					JOptionPane.showMessageDialog(null, "Error: " + e2);
+				}
+				}
+				catch (Exception e3){
+					JOptionPane.showMessageDialog(null, "Ju lutem klikoni një produkt!!!");
+				}
+				
+				
+				
+				
+			}
+		});
+		menuItem.setBackground(Color.WHITE);
+		menuItem.setIcon(new ImageIcon(MainPage.class.getResource("/images/edit.jpg")));
+		popupMenu.add(menuItem);
 		
 		
 		
@@ -339,13 +584,13 @@ public class MainPage {
 		lblData.setBounds(1266, 11, 78, 14);
 		pnlStatusBar.add(lblData);
 		
-		
-		
-		
+	
 		
 		// call function 'clock' to show date and time!
 		
 		clock();
+		
+		
 	}
 	
 	public void updateTable() {
@@ -364,4 +609,30 @@ public class MainPage {
 		}
 
 	}
+	
+	
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+
+	}
+	
+	
+	
+	
 }
